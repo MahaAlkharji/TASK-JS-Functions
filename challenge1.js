6faa9815c6f3edf7efc4dd714a983ce53af0219f
maha_alkharji@hotmail.com <maha_alkharji@hotmail.com>
2023-12-04 19:29:29 +0300

    s1

--- challenge1.js
+++ challenge1.js
@@ -4,6 +4,11 @@
  * - that just prints your name on the screen
  */
 
+function printName(name) {
+  console.log(`My name is ${name}`);
+}
+printName("Maha");
+
 /**
  * Task 2:
  * Create a function named `printAge`
@@ -11,6 +16,11 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+
+function printAge(BD) {
+  console.log(`My age: ${2023 - BD}`);
+}
+printAge(1998);
 
 /**
  * Task 3:
@@ -23,9 +33,35 @@
  * -- tr: it should print `Merhaba NAME`
  */
 
+function printHello(name, language) {
+  if (language == "en") {
+    console.log(`Hello ${name}`);
+  } else if (language == "es") {
+    console.log(`Hola ${name}`);
+  } else if (language == "fr") {
+    console.log(`Bonjour ${name}`);
+  } else if (language == "tr") {
+    console.log(`Merhaba ${name}`);
+  }
+}
+
+printHello("Maha", "es");
+printHello("Maha", "fr");
+
 /**
  * Task 4:
  * Create a function named `printMax`
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
  */
+
+function printMax(n1, n2) {
+  if (n1 > n2) {
+    console.log(n1);
+  } else {
+    console.log(n2);
+  }
+}
+
+printMax(5, 10);
+printMax(30, 1);
